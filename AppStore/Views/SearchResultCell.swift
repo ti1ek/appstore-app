fa73be165e5a4bf624adbbcac0103c366364bf9d
tkoszhanov <tilek.koszhanov@mail.ru>
2022-10-19 21:13:29 +0500

    Refactoring StackView

--- AppStore/Views/SearchResultCell.swift
+++ AppStore/Views/SearchResultCell.swift
@@ -60,16 +60,14 @@ class SearchResultCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        //backgroundColor = .yellow
         
-        let labelsStackView = UIStackView(arrangedSubviews: [
-        nameLabel, categoryLabel, ratingLabel
-        ])
-        
-        labelsStackView.axis = .vertical
         
         let infoTopStackView = UIStackView(arrangedSubviews: [
-        appIconimageView, labelsStackView, getButton
+        appIconimageView,
+        VerticalStackView(arrangedSubviews: [
+            nameLabel, categoryLabel, ratingLabel
+        ]),
+        getButton
         ])
         
         infoTopStackView.spacing = 12
@@ -81,28 +79,17 @@ class SearchResultCell: UICollectionViewCell {
         screenshotsStackView.spacing = 12
         screenshotsStackView.distribution = .fillEqually
         
-        let overallStackView = UIStackView(arrangedSubviews: [
-        infoTopStackView, screenshotsStackView
-        ])
-        overallStackView.axis = .vertical
-        overallStackView.spacing = 16
+        let overallStackView = VerticalStackView(arrangedSubviews: [
+            infoTopStackView, screenshotsStackView
+        ], spacing: 16)
         
         addSubview(overallStackView)
         
         overallStackView.fillSuperview(padding: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
         )
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-//        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-//        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
-//        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
-
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-
 }
