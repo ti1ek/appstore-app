a3898a29347dbbbabed886ab805befe2d958a0dc
tkoszhanov <tilek.koszhanov@mail.ru>
2022-10-19 22:16:25 +0500

    Model is created for network data

--- AppStore/Controllers/SearchController.swift
+++ AppStore/Controllers/SearchController.swift
@@ -14,7 +14,32 @@ class SearchController: UICollectionViewController, UICollectionViewDelegateFlow
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: identifier)
+        collectionView.register(SearchResultCell.self,
+                                forCellWithReuseIdentifier: identifier)
+        
+        fetchITunesApps()
+    }
+    
+    fileprivate func fetchITunesApps() {
+        let urlString = "https://itunes.apple.com/search?term=instagram&entity=software"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Failed to fetch apps", error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let searchResult = try JSONDecoder().decode(SearchResult.self, from: data)
+                
+                searchResult.results.forEach({print($0.trackName, $0.primaryGenreName)})
+            } catch let jsonError {
+                print("Failed to fetch json", jsonError)
+            }
+        }.resume()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -28,7 +53,8 @@ class SearchController: UICollectionViewController, UICollectionViewDelegateFlow
 
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! SearchResultCell
+        cell.nameLabel.text = "HERE IS MY APP NAME"
         return cell
     }
     
---
+++ AppStore/Model/SearchResult.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResult.swift
+//  AppStore
+//
+//  Created by Tilek Koszhanov on 10/19/22.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let resultCount: Int
+    let results: [Result]
+}
+
+struct Result: Decodable {
+    let trackName: String
+    let primaryGenreName: String
+}
