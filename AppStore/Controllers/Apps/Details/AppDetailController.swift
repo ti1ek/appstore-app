b3ee3432b1f8cb106038a571fa8e33ead9a11747
tkoszhanov <tilek.koszhanov@mail.ru>
2022-10-31 05:49:02 +0500

    App Info Auto Sizing of Cells is configured

--- AppStore/Controllers/Apps/Details/AppDetailController.swift
+++ AppStore/Controllers/Apps/Details/AppDetailController.swift
@@ -14,12 +14,17 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
             let urlString = "https://itunes.apple.com/lookup?id=\(appId ?? "")"
             
             Service.shared.fetchGenericJSONData(urlString: urlString) { (result: SearchResult?, error) in
-                print(result?.results.first?.releaseNotes)
+                let app = result?.results.first
+                self.app = app
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
             }
         }
     }
     
     let detailCellId = "detailCellId"
+    var app: Result?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,11 +42,19 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
+        cell.app = app
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return .init(width: view.frame.width, height: 300)
+        
+        let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000)) 
+        dummyCell.app = app
+        dummyCell.layoutIfNeeded()
+        
+        let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+        
+        return .init(width: view.frame.width, height: estimatedSize.height)
     }
     
 }
